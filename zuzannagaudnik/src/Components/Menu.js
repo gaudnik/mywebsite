a90113a772b770574123f948274c5a2df2edb432
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2023-05-29 18:55:11 +0200

    menu

--- zuzannagaudnik/src/Components/Menu.js
+++ zuzannagaudnik/src/Components/Menu.js
@@ -11,12 +11,17 @@ const Menu = () => {
               <span></span>
             </label>
             <ul className='menu__nav'>
-              <li className="menu__element"><Link to="/">Start</Link></li>
-              <li className="menu__element"><Link to="aboutme" >About me</Link></li>
-              <li className="menu__element"><Link to="tools">My tools</Link></li>
-              <li className="menu__element"><Link to="portfolio">Projects</Link></li>
-              <li className="menu__element"><Link to="contact">Contact</Link></li>
-
+              <li className="menu__element">
+                <Link to="/" onClick={() => document.getElementById('menu__btn').checked = false}>Start</Link>
+              </li>              
+              <li className="menu__element">
+                <Link to="aboutme" onClick={() => document.getElementById('menu__btn').checked = false}>About me</Link></li>
+              <li className="menu__element">
+                <Link to="tools" onClick={() => document.getElementById('menu__btn').checked = false}>My tools</Link></li>
+              <li className="menu__element">
+                <Link to="projects" onClick={() => document.getElementById('menu__btn').checked = false}>Projects</Link></li>
+              <li className="menu__element">
+                <Link to="contact" onClick={() => document.getElementById('menu__btn').checked = false}>Contact</Link></li>
             </ul>
         </nav>
       )
