94dbe1a6a593dd9a16d2c49c6a3c79e13e2316e3
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2023-07-31 14:16:55 +0200

    refactor

--- zuzannagaudnik/src/Components/Footer.js
+++ zuzannagaudnik/src/Components/Footer.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <h3 className="footer__header">©2023 Zuzanna Gaudnik · All rights reserved.</h3>
-        </div>
-    )
-}
+  const footerRef = useRef(null);
+  const [footerHeight, setFooterHeight] = useState(0);
+
+  useEffect(() => {
+    setFooterHeight(footerRef.current.clientHeight);
+  }, []);
+
+  return (
+    <div ref={footerRef} className="footer" style={{ marginTop: footerHeight }}>
+      <h3 className="footer__header">©2023 Zuzanna Gaudnik · All rights reserved.</h3>
+    </div>
+  );
+};
 
 export default Footer;
